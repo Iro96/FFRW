6925c31effca06ceba142815ed3d06e5e55602d9
Iro96 <bruh8080p@gmail.com>
2025-08-21 08:28:12 +0700

    update training.py at import modules

--- evofusion/utils/training.py
+++ evofusion/utils/training.py
@@ -1,8 +1,8 @@
 import torch
 import torch.nn as nn
-from ..core.adapter import KnowledgeAdapter
-from ..core.meta_controller import MetaController
-from ..core.fitness import fitness_score
+from evofusion.core.adapter import KnowledgeAdapter
+from evofusion.core.meta_controller import MetaController
+from evofusion.core.fitness import fitness_score
 
 def train(student, teacher, data_loader, epochs=10, lr=1e-3):
     optimizer = torch.optim.Adam(student.parameters(), lr=lr)
