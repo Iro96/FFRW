a067149a6eccd4c6c458b4bcf9b52a54f2a9c5fc
Iro96 <bruh8080p@gmail.com>
2025-08-21 08:18:34 +0700

    create demo version

---
+++ experiments/demo.py
@@ -0,0 +1,17 @@
+import torch
+from torch.utils.data import DataLoader, TensorDataset
+from evofusion.core.student import EvoStudent
+from evofusion.utils.training import train
+
+# Dummy dataset
+X = torch.randn(1000, 10)
+Y = torch.randn(1000, 1)
+dataset = TensorDataset(X, Y)
+loader = DataLoader(dataset, batch_size=32)
+
+# Teacher & Student
+teacher = EvoStudent(10, 1, hidden_layers=[128,128])
+student = EvoStudent(10, 1, hidden_layers=[32])
+
+# Train with EvoFusion
+train(student, teacher, loader, epochs=20)
